25315f3ae11f384109afaceee9cf547dbb385940
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-07-12 17:54:55 +0530

    Finished

--- client/src/components/POpUp.jsx
+++ client/src/components/POpUp.jsx
@@ -1,9 +1,25 @@
+import { baseURL } from "@/utils/constant"
+import axios from "axios"
 import { useState } from "react"
 
 
 export const POpUp = ({id,text,setUpdateUI,setOpen,open}) => {
 
     const [textvalue,setTextValue]=useState(text)
+
+    const updateTodo=()=>{
+      axios.patch(`${baseURL}/editTodo/${id}`,{
+        text: textvalue
+      })
+
+      .then(res=>{
+        setTextValue("")
+        setUpdateUI((prevState) => !prevState)
+     })
+     .catch(err=>{
+       console.log(err);
+     })
+    }
 
   return (
     <div className="mt-1">
@@ -12,7 +28,10 @@ export const POpUp = ({id,text,setUpdateUI,setOpen,open}) => {
           className="py-2 px-3 rounded-md outline-none w-[50%] bg-accent mr-2 "
         />
         <button className="py-2 px-2 bg-accentDark rounded-md hover:bg-accent"
-          onClick={()=> setOpen(!open)}
+          onClick={()=> {
+            setOpen(!open) ;
+            updateTodo()
+          }}
         >save</button>
     </div>
   )
